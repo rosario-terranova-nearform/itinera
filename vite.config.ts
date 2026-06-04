5e02460d7876d17de04a875f631211cb669502f1
rosario-terranova-nearform <rosario.terranova@nearform.com>
2026-06-04 10:17:29 +0200

    feat(T0.3): integrate Prettier for code formatting and update ESLint configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
